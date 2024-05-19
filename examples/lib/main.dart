6bc3d1e8309b8186e3754ed4099c56aef1295299
Tim McMaster <tim@mcmaster.id.au>
2024-05-19 22:48:19 +1000

    Added the Material 3 demo code to the src directory and updated the examples demo.

--- examples/lib/main.dart
+++ examples/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:wt_material_3_demo/my_app.dart';
+import 'package:wt_material_3_demo/wt_material_3_demo.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Material3DemoApp());
 }
--- lib/wt_material_3_demo.dart
+++ lib/wt_material_3_demo.dart
@@ -1,1 +1,4 @@
+/// Material 3 Demo
 library wt_material_3_demo;
+
+export 'src/material_3_demo_app.dart';
